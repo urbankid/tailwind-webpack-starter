fed98d717f057880d6911c454832249e958b1406
Alexander Morales <a@mojitoo.com>
2021-05-12 18:44:14 -0500

    Add index template

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tailwind with Webpack
+
+A simple repo setup of TailwindCSS 2 with Webpack 5
+
+## Usage
+
+```bash
+git clone https://github.com/urbankid/tailwind-webpack.git
+cd tailwind-webpack/
+npm install
+npm run dev:watch
+```
+
+This will open up localhost:8080.
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/app.js"),
@@ -23,4 +24,9 @@ module.exports = {
     contentBase: path.resolve(__dirname, "dist"),
     open: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
